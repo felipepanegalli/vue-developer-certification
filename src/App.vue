84ae29fd8277b9dca9ff9cb32000c70566119899
Felipe Panegalli <felipe.panegalli@gmail.com>
2023-06-23 10:25:33 -0300

    VC-04 feat: finished C1EP6

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="bg-gray-900 h-screen w-screen">
-    <MovieCard />
+  <div class="bg-gray-900 min-h-screen w-screen">
+    <div class="flex flex-wrap gap-2 gap-y-4 min-h-screen items-center justify-center">
+      <MovieCard v-for="item in db.items" :key="item.id" :movie="item" />
+    </div>
   </div>
 </template>
 
 <script setup>
 import MovieCard from './components/Cards/MovieCard.vue'
+import db from './composables/movies.json'
 
 </script>
 
--- src/components/Cards/MovieCard.vue
+++ src/components/Cards/MovieCard.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-
+  <div class="max-w-xs">
+    <div class="card-poster">
+      <img :src="movie.image" :alt="movie.name" class="h-96 card-w rounded-t-md">
+    </div>
+    <div class="card-footer bg-white px-2 py-4 card-w flex flex-col gap-2 h-44 justify-between rounded-b-md">
+      <div class="card-footer__info">
+        <h1>{{ movie.name }}</h1>
+        <div class="pb-2">
+          <span v-for="gender in movie.genres" :key="gender" class="mr-1 text-[10px] bg-indigo-400 text-white py-1 px-2 rounded-full">
+          {{ gender }}
+        </span>
+        </div>
+        <p class="text-[10px]">{{ movie.description }}</p>
+      </div>
+      <div class="card-footer__rating text-[10px]">
+        Rating: ({{ movie.rating }}/5) <span v-for="i in movie.rating" :key="i">⭐</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,4 +27,8 @@ defineProps({
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card-w{
+  width: 272px;
+}
+</style>
