bac7b4af51daa2d5f3809a44900e5b289a5e8b9b
Felipe Panegalli <felipe.panegalli@gmail.com>
2023-06-23 11:44:01 -0300

    VC-05 feat: add interactive rating

--- src/components/Cards/MovieCard.vue
+++ src/components/Cards/MovieCard.vue
@@ -7,28 +7,37 @@
       <div class="card-footer__info">
         <h1>{{ movie.name }}</h1>
         <div class="pb-2">
-          <span v-for="gender in movie.genres" :key="gender" class="mr-1 text-[10px] bg-indigo-400 text-white py-1 px-2 rounded-full">
-          {{ gender }}
-        </span>
+          <span v-for="gender in movie.genres" :key="gender"
+            class="mr-1 text-[10px] bg-indigo-400 text-white py-1 px-2 rounded-full">
+            {{ gender }}
+          </span>
         </div>
         <p class="text-[10px]">{{ movie.description }}</p>
       </div>
-      <div class="card-footer__rating text-[10px]">
-        Rating: ({{ movie.rating }}/5) <span v-for="i in movie.rating" :key="i">⭐</span>
+      eab30a
+      <div class="card-footer__rating text-[10px] flex items-center">
+        Rating: ({{ movie.rating }}/5)
+        <span @click="() => { }" class="flex">
+          <StarIcon color="#888888" width="16" height="16" v-for="i in movie.rating" :key="i" />
+        </span>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import StarIcon from '../Icons/StarIcon.vue';
 
 defineProps({
   movie: Object
 })
+
+const rate = ref(0)
 </script>
 
 <style lang="scss" scoped>
-.card-w{
+.card-w {
   width: 272px;
 }
 </style>
---
+++ src/components/Icons/StarIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="width" :height="height" viewBox="0 0 24 24">
+    <path :fill="color"
+      d="m5.825 22l1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625l7.2.625l-5.45 4.725L18.175 22L12 18.275L5.825 22Z" />
+  </svg>
+</template>
+
+<script setup>
+defineProps({
+  color: { type: String, default: "#888888" },
+  width: { type: String, default: "32" },
+  height: { type: String, default: "32" },
+})
+</script>
+
+<style lang="scss" scoped></style>
