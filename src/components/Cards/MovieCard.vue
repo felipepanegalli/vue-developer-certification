e4faf138be7380360dfaf2e43efe70c6d57f77eb
Felipe Panegalli <felipe.panegalli@gmail.com>
2023-06-23 22:12:53 -0300

    VC-07 feat: finished the rating banner C01EP10

--- src/components/Cards/MovieCard.vue
+++ src/components/Cards/MovieCard.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="max-w-xs">
-    <div class="card-poster">
+    <div class="card-poster relative">
+      <h1 class="absolute text-[10px] top-[14px] right-[17px] z-10 text-yellow-800">{{ hasRating }}</h1>
+      <StarIcon color="#eab30a" width="30" height="30" class="absolute top-1 right-1 drop-shadow-md" />
       <img :src="movie.image" :alt="movie.name" class="h-96 card-w rounded-t-md">
     </div>
     <div class="card-footer bg-white px-2 py-4 card-w flex flex-col gap-2 h-44 justify-between rounded-b-md">
@@ -15,7 +17,7 @@
         <p class="text-[10px]">{{ movie.description }}</p>
       </div>
       <div class="card-footer__rating text-[10px] flex items-center ">
-        <span>Rating: ({{ movie.rating }}/5)</span>
+        <span>Rating: ({{ hasRating }}/5)</span>
         <span class="flex">
           <StarIcon v-for="i in 5" :key="i" :color="i <= movie.rating ? '#eab30a' : '#888888'" width="16" height="16"
             class="cursor-pointer" @click="setRating(i)" />
@@ -26,6 +28,7 @@
 </template>
 
 <script setup>
+import { computed } from 'vue';
 import StarIcon from '../Icons/StarIcon.vue';
 
 const props = defineProps({
@@ -35,6 +38,10 @@ const props = defineProps({
 const setRating = (val) => {
   props.movie.rating = val
 }
+
+const hasRating = computed(() => {
+  return (props.movie.rating === 0 || props.movie.rating === null) ? ' - ' : props.movie.rating
+})
 </script>
 
 <style lang="scss" scoped>
